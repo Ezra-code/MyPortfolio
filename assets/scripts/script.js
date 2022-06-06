a02f2755f1cb46c76fa3f08db1d30389d5315be4
Ezra-code <njerukithinji872@gmail.com>
2022-06-06 09:15:03 +0300

    change javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,39 +1,39 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-    });
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scrollY > 20){
+//             $('.navbar').addClass("sticky");
+//         }else{
+//             $('.navbar').removeClass("sticky");
+//         }
+//     });
 
-    //toggle menu
-    $('.menu-btn').click(function(){
+//     //toggle menu
+//     $('.menu-btn').click(function(){
 
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+//         $('.navbar .menu').toggleClass("active");
+//         $('.menu-btn i').toggleClass("active");
+//     });
 
-    //owl carousel
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items:1,
-                nav: false
-            },
-            600:{
-                items:2,
-                nav: false
-            },
-            1000:{
-                items:3,
-                nav: false
-            }
-        }     
+//     //owl carousel
+//     $('.carousel').owlCarousel({
+//         margin: 20,
+//         loop: true,
+//         autoplayTimeout: 2000,
+//         autoplayHoverPause: true,
+//         responsive: {
+//             0:{
+//                 items:1,
+//                 nav: false
+//             },
+//             600:{
+//                 items:2,
+//                 nav: false
+//             },
+//             1000:{
+//                 items:3,
+//                 nav: false
+//             }
+//         }     
         
-    })
-});
+//     })
+// });
